6a754e3bb6de7cf89adf41c3732fb793bc0f0787
mohtih kumar <mohith.kumar@spurtreetech.com>
2026-03-14 12:38:33 +0530

    merge conflict5

--- flutter_app/lib/services/api_service.dart
+++ flutter_app/lib/services/api_service.dart
@@ -4,16 +4,8 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   // ✅ Mac + iOS Simulator / Chrome:
-<<<<<<< HEAD
-  static const String baseUrl = 'http://10.1.107.243:5001/api';
+  static const String baseUrl = 'http://192.168.137.181:5001/api';
   // 🤖 Android Emulator — uncomment this instead:
-=======
-  // ✅ Physical device (WiFi) — investor demo:
-  static const String baseUrl = 'http://10.1.107.243:5001/api';
-  // 💻 Mac + iOS Simulator / Chrome:
-  // static const String baseUrl = 'http://localhost:5001/api';
-  // 🤖 Android Emulator:
->>>>>>> 3661d86c2612725926424305118773f515585db0
   // static const String baseUrl = 'http://10.0.2.2:5001/api';
 
   static Map<String, String> _headers({String? token, bool json = true}) {
